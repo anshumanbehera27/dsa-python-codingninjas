b081ace43718d4c330110ed5de8879eefe0b2bd1
Anshuman behera <90245492+dhjhc@users.noreply.github.com>
2022-01-13 21:48:18 +0530

    Update array is sorted or not.py

--- recursion/array is sorted or not.py
+++ recursion/array is sorted or not.py
@@ -1,1 +1,12 @@
-
+def sort(arr,n):
+    if n == 0 or n == 1 :
+        return True
+    if arr[0] > arr[1]:
+        return False
+    return sort(arr[1:],n-1)
+arr = [int(x)for x in input().split()]
+n = int(input())
+if sort(arr,n):
+    print("yes")
+else:
+    print("no" )
